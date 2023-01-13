ab26a7cb80a810b09e2df0fe249f3eb8c8f835f8
Marta <mortizfe11@gmail.com>
2023-01-13 09:00:34 +0000

    añadimos carpeta de static global

--- hello_world/hello_world/settings.py
+++ hello_world/hello_world/settings.py
@@ -120,6 +120,10 @@ STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
