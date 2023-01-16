743d5c521abc1d23aef121072a8f41fda5d54ba8
Marta <mortizfe11@gmail.com>
2023-01-16 12:52:12 +0100

    added slughash

--- hello_world/hello_world_app/admin.py
+++ hello_world/hello_world_app/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Member
-
 # Register your models here.
 
 class MemberAdmin(admin.ModelAdmin):
     list_display = ("id","firstname", "lastname", "phone", "joined_date")
-    prepopulated_fields = {"slug": ("firstname", "lastname")} #auto-rellena el campo slug con firstname-lastname
+    prepopulated_fields = {"slug": ("firstname", "lastname", "slugh_hash")} #auto-rellena el campo slug con firstname-lastname
 
 admin.site.register(Member, MemberAdmin)
--- hello_world/hello_world_app/models.py
+++ hello_world/hello_world_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+def generate_slug_hash():
+    return str(uuid.uuid1())[:8]
 
 class Member(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -8,6 +12,7 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.CharField(max_length=20, null=True)
     joined_date = models.DateField(null=True)
+    slugh_hash = models.CharField(max_length=8,default=generate_slug_hash, null=False)
     slug = models.SlugField(default="", null=False)
 
     def __str__(self):
